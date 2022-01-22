22287a65ce47a676099c809a6d9b869f268eef14
kowasaur <75297283+kowasaur@users.noreply.github.com>
2022-01-22 21:46:28 +1000

    addition

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,58 @@ import { ChangeEvent, useEffect, useRef, useState } from "react";
 
 export default () => {
     const [input, setInput] = useState("");
+    const [result, setResult] = useState<number>();
     const [inputFocused, setInputFocused] = useState(false);
     const textarea = useRef<HTMLTextAreaElement>(null);
 
+    function isNumber(numberish: string) {
+        return !isNaN(numberish as unknown as number);
+    }
+
+    function evaluateLatex(latex: string) {
+        let stack: number[] = [];
+        let i = 0;
+        console.log("LATEX: ", latex);
+
+        while (i < latex.length) {
+            const char = latex[i];
+
+            if (isNumber(char)) {
+                let buffer = "";
+                do {
+                    buffer += latex[i];
+                } while (isNumber(latex[++i]));
+                stack.push(parseInt(buffer));
+            } else if (char === "+") {
+                if (++i < latex.length) {
+                    let buffer = "";
+                    do {
+                        buffer += latex[i];
+                    } while (isNumber(latex[++i]));
+
+                    const b = parseInt(buffer);
+                    const a = stack.pop();
+                    if (a === undefined) throw new Error("plus without is missing operand");
+                    console.log("PLUS", a, b);
+                    stack.push(a + b);
+                }
+            } else {
+                i++;
+            }
+        }
+
+        return stack.pop();
+    }
+
     function handleInput(e: ChangeEvent<HTMLTextAreaElement>) {
-        setInput(e.target.value);
+        const text = e.target.value;
+
+        if (text[text.length - 1] == "\n") {
+            console.log("poggers");
+        }
+
+        setInput(text);
+        setResult(evaluateLatex(text));
     }
 
     function focusInput() {
@@ -27,8 +74,13 @@ export default () => {
                 ref={textarea}
                 onBlur={() => setInputFocused(false)}
             />
-            <Latex>{`$ ${input}$`}</Latex>
+            <Latex>{`$ ${input} $`}</Latex>
             {inputFocused && <div className="cursor" />}
+            {result && (
+                <div className="result">
+                    <Latex>{`$ ${result} $`}</Latex>
+                </div>
+            )}
         </div>
     );
 };
