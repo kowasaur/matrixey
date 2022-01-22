4e6764d578456c22e7f0a028c95d84e40c46bb17
kowasaur <75297283+kowasaur@users.noreply.github.com>
2022-01-22 19:42:40 +1000

    basic latex input

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,34 @@
 import "katex/dist/katex.min.css";
 import Latex from "react-latex-next";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 
 export default () => {
+    const [input, setInput] = useState("");
+    const [inputFocused, setInputFocused] = useState(false);
+    const textarea = useRef<HTMLTextAreaElement>(null);
+
+    function handleInput(e: ChangeEvent<HTMLTextAreaElement>) {
+        setInput(e.target.value);
+    }
+
+    function focusInput() {
+        textarea.current?.focus();
+        setInputFocused(true);
+    }
+
+    useEffect(focusInput, []);
+
     return (
-        <Latex strict>{`$
-        \\begin{bmatrix}
-            a \\\\
-            b
-        \\end{bmatrix}
-        $`}</Latex>
+        // tabIndex makes focus work on divs
+        <div className="input-box" tabIndex={0} onFocus={focusInput}>
+            <textarea
+                onChange={handleInput}
+                value={input}
+                ref={textarea}
+                onBlur={() => setInputFocused(false)}
+            />
+            <Latex>{`$ ${input}$`}</Latex>
+            {inputFocused && <div className="cursor" />}
+        </div>
     );
 };
